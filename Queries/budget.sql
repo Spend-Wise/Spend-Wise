bd9b838b0a5ff24f2b1303e50e6a59296b7263f8
devDurgeshK <durgeshkavate.7@outlook.com>
2024-03-22 22:06:24 +0530

    commit #21 - updated budget.sql

--- Queries/budget.sql
+++ Queries/budget.sql
@@ -1,5 +1,4 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "budget" (
+CREATE TABLE "budget" (
 	"budget_id"	INTEGER NOT NULL UNIQUE,
 	"user_id"	INTEGER NOT NULL,
 	"budget_name"	TEXT NOT NULL,
@@ -13,6 +12,3 @@ CREATE TABLE IF NOT EXISTS "budget" (
 	PRIMARY KEY("budget_id" AUTOINCREMENT),
 	FOREIGN KEY("user_id") REFERENCES "registration"("user_id")
 );
-INSERT INTO "budget" ("budget_id","user_id","budget_name","budget_amount","budget_category","budget_status","date","time","expense_limit","archived") VALUES (1,1,'bud 1',770000,NULL,700000,'19/3/2024','10:32:8',700000,'no'),
- (2,1,'sd',50000,'Grocery',5780,'19/3/2024','11:27:50',7500,'no');
-COMMIT;
